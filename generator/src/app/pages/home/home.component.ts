9581477c55c0e325fd27786e52a78cb57b797aea
Grayson McClead <gmcclead@sportsbusinessjournal.com>
2024-08-25 18:46:58 -0500

    start of markov genrator

--- generator/src/app/pages/home/home.component.ts
+++ generator/src/app/pages/home/home.component.ts
@@ -8,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.sass'
 })
 export class HomeComponent {
-
+  names = ["Grayson", "Jennifer", "Haley", "Ben", "Brady", "Jake", "Quinn", "Zoe", "Xavier"]
+  calculateProbability(){
+    let characters:any = []
+    for(let name of this.names){
+      for(let i = 0; i < name.length; i++){
+          if(characters.includes(name.charAt(i))){
+            
+          }
+      }
+    }
+  }
 }
