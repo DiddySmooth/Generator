0d6e1f824522ae60769a8a959b4a1f0fd5a9d6bf
Grayson McClead <gmcclead@sportsbusinessjournal.com>
2024-08-25 19:15:01 -0500

    letter probablity calc

--- generator/src/app/pages/home/home.component.ts
+++ generator/src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,16 +7,30 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.sass'
 })
-export class HomeComponent {
-  names = ["Grayson", "Jennifer", "Haley", "Ben", "Brady", "Jake", "Quinn", "Zoe", "Xavier"]
+export class HomeComponent implements OnInit{
+
+  ngOnInit(): void {
+    this.calculateProbability()
+  }
+  names = ["Grayson", "Jennifer", "Haley", "Ben", "Brady", "Jake", "Quinn", "Zoe", "Xavier", "Greyson"]
   calculateProbability(){
     let characters:any = []
     for(let name of this.names){
       for(let i = 0; i < name.length; i++){
-          if(characters.includes(name.charAt(i))){
-            
+          if(characters.some((obj: any) => obj.letter1 == name.charAt(i) && obj.letter2 == name.charAt(i + 1))){
+            characters = characters.map((m: any) =>
+              m.letter1 === name.charAt(i) && m.letter2 === name.charAt(i + 1) ? { ...m, count: m.count + 1 } : m
+            );
+          }else{
+            let mapping:any = {
+              letter1: name.charAt(i),
+              letter2: name.charAt(i+1),
+              count: 1
+            }
+            characters.push(mapping)
           }
       }
     }
+    console.log(characters)
   }
 }
